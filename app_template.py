68a5a4fe3accdb4f87c6bb9d6036d08fc9fd6181
forrestcasey <forrestpcasey@gmail.com>
2021-11-26 17:41:23 -0600

    deliv1 & 2

--- app_template.py
+++ app_template.py
@@ -1,14 +1,15 @@
 #create flask dependency
-from flask import Flask
+# from flask import Flask
 
 #create new flask instance
-app = Flask(__name__)
+# app = Flask(__name__)
 
 #create first route
 #create function
 #when making route in flask, put code below @app.route('/')
-@app.route('/')
-def hello_world():
-        return 'Hello world'
+
+# @app.route('/')
+# def hello_world():
+        # return 'Hello world'
 
 
